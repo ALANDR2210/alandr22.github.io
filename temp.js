478e9658c0585f45b5a6a6a776729cc85b9cc1bc
alandr22 <45125579+alandr22@users.noreply.github.com>
2018-12-04 17:07:17 -0300

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,22 @@
+var imageSF = document.querySelector('.trip-image');
+var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+
+function openModal() {
+    backdrop.style.display = 'block';
+    modal.style.display = 'block';
+}
+
+function closeModal () {
+    backdrop.style.display = 'none';
+    modal.style.display = 'none';
+}
+
+imageSF.onclick = openModal;
+backdrop.onclick = closeModal;
+
+$("#myModal").on('backdrop.onclick', function (e) {
+    $("#myModal iframe").attr("src", $("#myModal iframe").attr("src"));
+});
+
+console.log(imageSF);
