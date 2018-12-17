61275e75790418b16a075ac7b47566fdd8f02a1c
alandr22 <45125579+alandr22@users.noreply.github.com>
2018-12-17 18:42:02 -0300

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,5 @@
 const mq = window.matchMedia( "(max-width: 767px)" );
+const mm = window.matchMedia( "(min-width: 767px)" );
 function toggleNav() {
     var element = document.getElementById("myNav");
     var p1 = document.getElementById("pl1");
@@ -18,31 +19,30 @@ function toggleNav() {
         element.style.height = "0%";
 
     } else {
-        p1.style.display = "inline"
         p1.style.color = "#fff"
         p1.style.transition = "0.8s"
         p1.style.position = "relative";
-        p1.style.top = "-120px"
+        p1.style.top = "-150px"
         p1.style.right = "0px"
-        p1.style.fontSize = "46px"
+        p1.style.fontSize = "30px"
         p2.style.color = "#fff"
         p2.style.transition = "0.8s"
         p2.style.position = "relative"
-        p2.style.top = "-120px"
+        p2.style.top = "-150px"
         p2.style.right = "0px"
-        p2.style.fontSize = "46px"
+        p2.style.fontSize = "30px"
         p3.style.color = "#fff"
         p3.style.transition = "0.8s"
         p3.style.position = "relative";
-        p3.style.top = "-120px"
+        p3.style.top = "-150px"
         p3.style.right = "0px"
-        p3.style.fontSize = "46px"
+        p3.style.fontSize = "30px"
         p4.style.color = "#fff"
         p4.style.transition = "0.8s"
         p4.style.position = "relative";
-        p4.style.top = "-120px"
+        p4.style.top = "-150px"
         p4.style.right = "0px"
-        p4.style.fontSize = "46px"
+        p4.style.fontSize = "30px"
         element.style.height = "100%";
     }
 }
